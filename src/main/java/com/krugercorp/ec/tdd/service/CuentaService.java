cdb4141b1d1da73b9a2c02e7828c987ebd3cc5bd
Paúl Alarcón <pag66.06@gmail.com>
2019-02-22 12:51:30 -0800

    Actualizando bug

--- src/main/java/com/krugercorp/ec/tdd/service/CuentaService.java
+++ src/main/java/com/krugercorp/ec/tdd/service/CuentaService.java
@@ -1,9 +1,8 @@
 package com.krugercorp.ec.tdd.service;
 
-import java.lang.StackWalker.Option;
 import java.util.Optional;
 
-import com.krugercorp.ec.tdd.model.Cuenta;
+
 
 public interface CuentaService {
 
